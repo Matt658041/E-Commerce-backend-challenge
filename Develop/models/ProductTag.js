1d5d73d9c9f32d8f75d94f64fbd1cde6e24572f6
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-05-05 12:19:10 -0500

    finished models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -13,10 +13,22 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_tag_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model:'product',
+        key: 'id'
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: 'tag',
+        key: 'id'
+    }
+}
   },
   {
     sequelize,
