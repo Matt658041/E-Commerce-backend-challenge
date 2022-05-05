967a1473ab3ec1b9868e4a53f82ab9fc4fe4c48f
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-05-05 14:04:11 -0500

    finished tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,27 +2,119 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // find all categories
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock'
+        ]
+      },
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // find one category by its `id` value
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes:[
+      'id',
+      'tag_name'
+    ],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock'
+        ]
+      },
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // create a new category
+  Tag.create({
+    Tag_name:req.body.Tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // update a category by its `id` value
+  Tag.update({
+    Tag_name: req.body.Tag_name
+  },
+  {
+    where:{ id: req.params.id}
+  }
+  )
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete a category by its `id` value//
+/////Warning our Teacher's Assistant said it was a terrible idea to delete data permenantly.////
+////Unless you can archive or something never, ever, do this.//////
+ Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
+
 
 module.exports = router;
